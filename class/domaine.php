44a1bb0073a4447220169839ac49a3f9febdafed
didshwa <shwa@shwa.be>
2016-02-07 23:50:24 -0500

    Update domaine.php

--- class/domaine.php
+++ class/domaine.php
@@ -1,2 +1,16 @@
 <?php
-
+  function getdomains()
+  {	global $ovh;
+    $res = $ovh->get('/domain');
+    return $res;
+  }
+  function getinfodomain($dom)
+  {	global $ovh;
+    $res = $ovh->get('/domain/'.$dom.'/serviceInfos');
+    return $res;
+  }
+  function getinfozonedomain($dom)
+  {	global $ovh;
+    $res = $ovh->get('/domain/zone/'.$dom.'/serviceInfos');
+    return $res;
+  }
