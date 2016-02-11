1a8a677000e68ce911de62f0a8357fd1a5f847c0
didshwa <shwa@shwa.be>
2016-02-11 02:04:26 -0500

    Update dyndns.php

--- test/dyndns.php
+++ test/dyndns.php
@@ -1,6 +1,9 @@
 <?php
 include("../init-test.php");
 
+$dom 		= "test.com";
+$subDomain 	= "home";
+
 if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
     $IP = $_SERVER['HTTP_X_FORWARDED_FOR'];
 }elseif(isset($_SERVER['HTTP_CLIENT_IP'])){
@@ -8,26 +11,19 @@ if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
 }else{
     $IP = $_SERVER['REMOTE_ADDR'];
 }
-$dom 		= "domaine.com";
-$subDomain 	= "home";
 
 $DynHost = new DynHost($ovh);
 $DynHost->setdomain($dom);
 $res_record_id = $DynHost->getdyndnsid($subDomain);
 $record_id = $res_record_id[0];
 
-if(empty($record_id)){
-	echo "vide";
-}else{
-	//echo $record_id;
+if(!empty($record_id)){
 	$res = $DynHost->getdyndnsdata($record_id);
-	
 	if ($IP == $res['ip'])
 	{
 		echo " \r\nL'ip n'a pas change.";
 	}else{
 		$DynHost->putdyndns($subDomain,$IP,$record_id);
+		$DynHost->refresh();
 	}
-	//print_r($res);
-	$DynHost->refresh();
 }
